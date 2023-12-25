2bfb4d9aacc98361219018fa571f48968d045a7b
Gustavo Henrique Tavares de Souza <gutvo10@gmail.com>
2023-12-25 20:24:01 -0300

    mudança nas configuraçõs do multer

--- BackEnd/src/multer/config.ts
+++ BackEnd/src/multer/config.ts
@@ -1,24 +1,24 @@
 import multer from 'multer'
-import { randomBytes } from 'crypto'
+import { v4 as uuidv4 } from 'uuid'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    if (file) {
-      cb(null, 'src/uploads')
+    if (file !== null || file !== undefined) {
+      cb(null, 'src/images')
     }
   },
   filename: function (req, file, cb) {
-    if (file) {
+    if (file !== null || file !== undefined) {
       // Extração da extensão do arquivo original:
       const extensaoArquivo = file.originalname.split('.')[1]
 
       // Cria um código randômico que será o nome do arquivo
-      const novoNomeArquivo = randomBytes(64).toString('hex')
+      const novoNomeArquivo = uuidv4()
 
       // Indica o novo nome do arquivo:
       cb(null, `${novoNomeArquivo}.${extensaoArquivo}`)
     }
-  },
+  }
 })
 
 export const upload = multer({ storage })
