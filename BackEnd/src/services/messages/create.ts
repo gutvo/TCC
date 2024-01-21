e097eced82d4716df0ff308ac971a613d0ed0339
Gustavo <gutvo10@gmail.com>
2024-01-21 16:10:09 -0300

    criação do serviço para as mensagens

---
+++ BackEnd/src/services/messages/create.ts
@@ -0,0 +1,23 @@
+import { Message } from '@Models/chats/messages'
+
+interface MessageProps {
+  id: string
+  email: string
+  message: string
+  room: string
+}
+
+interface CreateServiceProps {
+  message: MessageProps
+  roomId: string
+}
+
+export default async function createService ({ message, roomId }: CreateServiceProps) {
+  const result = await Message.create({
+    email: message.email,
+    message: message.message,
+    roomId
+  })
+
+  return { result }
+}
---
+++ BackEnd/src/services/messages/index.ts
@@ -0,0 +1,9 @@
+import createService from './create'
+import listService from './list'
+
+const MessagesServices = {
+  createService,
+  listService
+}
+
+export default MessagesServices
